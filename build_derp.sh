070bb6df48c254feea3d316a5e8d6a95d2aeeec6
FranticCoin <nikhilbaddi98@gmail.com>
2020-03-03 11:03:57 +0530

    derpfest : added repo sync command and configs for custom app packages

--- build_derp.sh
+++ build_derp.sh
@@ -1,5 +1,15 @@
 rm -rf out/target/product/land
 
+rm -rf vendor/gapps/config.mk
+rm -rf vendor/pixelstyle/config.mk
+rm -rf vendor/aosip/config/packages.mk
+
+repo sync -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+rm -rf vendor/gapps/config.mk && wget -P vendor/gapps/ https://raw.githubusercontent.com/FranticCoin/android_vendor_gapps/master/config.mk
+rm -rf vendor/pixelstyle/config.mk && wget -P vendor/pixelstyle/ https://raw.githubusercontent.com/FranticCoin/platform_vendor_pixelstyle/master/config.mk
+rm -rf vendor/aosip/config/packages.mk && wget -P vendor/aosip/config/ https://raw.githubusercontent.com/FranticCoin/platform_vendor_aosip/master/config/packages.mk
+
 . build/envsetup.sh
 lunch derp_land-userdebug
 mka kronic
