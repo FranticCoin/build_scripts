07e6a0a699fa2c88653ff24c23e1d82bbbe7ec03
FranticCoin <nikhilbaddi98@gmail.com>
2020-03-03 11:08:57 +0530

    extendedUI : added commands to clean out dir and sync repo

--- build_exui.sh
+++ build_exui.sh
@@ -1,3 +1,7 @@
+rm -rf out/target/product/land
+
+repo sync -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 . build/envsetup.sh
 export CUSTOM_BUILD_TYPE=OFFICIAL
 lunch aosp_land-userdebug
